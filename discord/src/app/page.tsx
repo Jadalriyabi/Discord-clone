3f92eb07018d5db712c61e22adae74df5bf1f265
Jad Alriyabi <71855107+Jadalriyabi@users.noreply.github.com>
2024-11-03 10:50:15 -0800

    update page.tsx

--- discord/src/app/page.tsx
+++ discord/src/app/page.tsx
@@ -1,7 +1,21 @@
-import Image from "next/image";
+import {use, useState} from "react";
+
+interface Message {
+  sender: string;
+  content: string;
+}
 
 export default function Home() {
-  return (
-    <div>Hello world</div>
-  );
+  const [messages, setMessages] = useState<Message[]>([
+    {sender: "Alice", content: "Hello, world!"},
+    {sender: "Bob", content: "Hello, Alice!" }
+  ]);
+
+  return messages.map((message, index) => 
+    (
+    <div key={index}>
+      <strong>{message.sender}</strong>: {message.content}
+      {}
+    </div>
+  ));
 }
